9ef6ac16739b3900c0702e999264295d1447ae93
Luffy20241 <168181882+Luffy20241@users.noreply.github.com>
2024-06-20 11:27:06 +0600

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "24464839"))
 API_HASH = os.environ.get("API_HASH", "c906bdd79dae0c7e6b8446db37128705")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002208828602"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001928455890"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1908235162"))
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sanjitha:sanjitha01@clust
 DB_NAME = os.environ.get("DATABASE_NAME", "bot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001928455890"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002208828602"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
